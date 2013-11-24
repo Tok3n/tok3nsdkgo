2c114f69bce8ef4568db9ab471b0b1f8d46aadfa
Jose Angel Espinoza Portillo <jose@ngel.es>
2013-11-24 15:45:21 -0600

    Adding reference to actual object

--- tok3n.go
+++ tok3n.go
@@ -60,6 +60,6 @@ func (t Tok3nInstance) getAccessUrl(callback, callbackdata string) (string,error
 	if err!= nil{
 		return "", err
 	}
-	return _addDomain(fmt.Sprintf("/login.do?publicKey=%s&session=%s&callbackurl=%s&callbackdata=%s",t.Config.PublicKey,session,callback,callbackdata)),nil
+	return t._addDomain(fmt.Sprintf("/login.do?publicKey=%s&session=%s&callbackurl=%s&callbackdata=%s",t.Config.PublicKey,session,callback,callbackdata)),nil
 
 }
