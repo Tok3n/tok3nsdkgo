905fcb3753f93e2319b536b34289fcd7a484c039
Jose Angel Espinoza Portillo <jose@ngel.es>
2013-11-24 15:07:03 -0600

    Asingment bugs

--- tok3n_appengine.go
+++ tok3n_appengine.go
@@ -6,8 +6,8 @@ import (
 )
 
 func GetAppEngineTok3nInstance(c appengine.Context, conf Tok3nConfig) *Tok3nInstance{
-	client = urlfetch.Client(c)
-	tok3n = new(Tok3nInstance)
+	client := urlfetch.Client(c)
+	tok3n := new(Tok3nInstance)
 	tok3n.Client = client
 	tok3n.Config = conf
 	return tok3n
